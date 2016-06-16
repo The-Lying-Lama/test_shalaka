427d6e9a120b7eb829ce33e9ba68324a7e057e7b
shalaka.kulkarni@zimmerbiomet.com <shalaka.kulkarni@zimmerbiomet.com>
2016-06-16 17:18:29 +0530

    test 2

--- test_shalaka/src/test_shalaka/S_Test1.java
+++ test_shalaka/src/test_shalaka/S_Test1.java
@@ -4,7 +4,7 @@ public class S_Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-
+        S_Test2 s1 = new S_Test2();
 	}
 
 }
---
+++ test_shalaka/src/test_shalaka/S_Test2.java
@@ -0,0 +1,9 @@
+package test_shalaka;
+
+public class S_Test2 {
+
+	S_Test2()
+	{
+		System.out.println("Test2");
+	}
+}
