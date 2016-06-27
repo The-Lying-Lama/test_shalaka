7d26db648f837d1bd14be84ed96d2992789623bf
shalaka.kulkarni@zimmerbiomet.com <shalaka.kulkarni@zimmerbiomet.com>
2016-06-27 16:59:42 +0530

    added 1 SOP

--- test_shalaka/src/test_shalaka/S_Test1.java
+++ test_shalaka/src/test_shalaka/S_Test1.java
@@ -5,6 +5,7 @@ public class S_Test1 {
 	public static void main(String[] args) {
 		System.out.println("Test");
         S_Test2 s1 = new S_Test2();
+        System.out.println("added 1 sop");
 	}
 
 }
