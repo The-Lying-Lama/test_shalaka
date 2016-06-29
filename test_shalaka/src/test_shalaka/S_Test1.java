3eb38373c5c464248462c5efc7550422eccc81bc
shalaka <s.k@test123.com>
2016-06-29 18:20:29 +0530

    adding to master

--- test_shalaka/src/test_shalaka/S_Test1.java
+++ test_shalaka/src/test_shalaka/S_Test1.java
@@ -7,6 +7,8 @@ public class S_Test1 {
         S_Test2 s1 = new S_Test2();
         System.out.println("added 1 sop");
         System.out.println("added feature 1 branch which is created from develop branch");
+System.out.println("using command prompt");
+System.out.println("test diff");
 	}
 
 }
