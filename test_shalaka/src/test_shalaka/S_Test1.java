190b68230b8bca80ce13bae1c604306a3954f669
shalaka <s.k@test123.com>
2016-06-30 15:48:40 +0530

    change 1

--- test_shalaka/src/test_shalaka/S_Test1.java
+++ test_shalaka/src/test_shalaka/S_Test1.java
@@ -5,10 +5,6 @@ public class S_Test1 {
 	public static void main(String[] args) {
 		System.out.println("Test");
         S_Test2 s1 = new S_Test2();
-        System.out.println("added 1 sop");
-        System.out.println("added feature 1 branch which is created from develop branch");
-System.out.println("using command prompt");
-System.out.println("test diff");
 	}
 
 }
