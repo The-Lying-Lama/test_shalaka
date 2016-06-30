6e7085b90cfec2d2c4302ad2884e2140e107ecb2
shalaka <s.k@test123.com>
2016-06-30 16:34:49 +0530

    commit1-feature1

--- test_shalaka/src/test_shalaka/S_Test1.java
+++ test_shalaka/src/test_shalaka/S_Test1.java
@@ -4,7 +4,8 @@ public class S_Test1 {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-        S_Test2 s1 = new S_Test2();
+          S_Test2 s1 = new S_Test2();
+System.out.println("New feature");
 	}
 
 }
